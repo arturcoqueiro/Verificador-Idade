43dc595a282bf1d0d759d38cae2bd029554ca579
Artur Coqueiro <rienstatic@outlook.com>
2022-01-02 08:20:37 -0300

    primeiro de 2022

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var 
+var anoAtual = new Date().getFullYear()
+var anoNasc = document.getElementById('ano')
+var sex = document.getElementsByName('sex')
+var gen = ''
+
+function ver(){
+    var age = anoAtual - Number(anoNasc.value)
+
+    if (Number(anoNasc.value) >= anoAtual || age > 120){
+        alert('Dados Inválidos, Tente Novamente')
+    }else{
+        if (sex[0].checked){
+            gen = 'um homem'
+        }else{
+            gen = 'uma mulher'
+        }
+        document.getElementById("res").innerHTML = `Detectamos ${gen} de ${age} anos`
+    }
+
+
+    
+}
