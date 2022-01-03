84975744f5c7878c255b92d6ffd328e20eca3c38
Artur Coqueiro <rienstatic@outlook.com>
2022-01-03 09:02:50 -0300

    final update

--- index.js
+++ index.js
@@ -9,12 +9,34 @@ function ver(){
     if (Number(anoNasc.value) >= anoAtual || age > 120){
         alert('Dados Inválidos, Tente Novamente')
     }else{
+        var img= document.createElement('img')
         if (sex[0].checked){
             gen = 'um homem'
+            if (age < 10){
+                img.setAttribute('src', 'imgs/foto-bebe-m.png')
+            }else if(age < 21){
+                img.setAttribute('src', 'imgs/foto-jovem-m.png')
+            }else if (age < 50){
+                img.setAttribute('src', 'imgs/foto-adulto-m.png')
+            }else{
+                img.setAttribute('src', 'imgs/foto-idoso-m.png')
+            }
         }else{
             gen = 'uma mulher'
+            if (age < 10){
+                img.setAttribute('src', 'imgs/foto-bebe-f.png')
+            }else if (age < 21){
+                img.setAttribute('src', 'imgs/foto-jovem-f.png')
+            }else if(age < 50){
+                img.setAttribute('src', 'imgs/foto-adulto-f.png')
+            }else {
+                img.setAttribute('src', 'imgs/foto-idoso-f.png')
+            }
         }
-        document.getElementById("res").innerHTML = `Detectamos ${gen} de ${age} anos`
+        //document.getElementById("res").innerHTML = `Detectamos ${gen} de ${age} anos`
+
+        res.innerHTML = `Detectamos ${gen} de ${age} anos`
+        res.appendChild(img)//Pq isso funciona????, Não criei uma variavel 'res' mas o JS pega o ID diretamente... E antes nao pegava qundo testei!!
     }
 
 
